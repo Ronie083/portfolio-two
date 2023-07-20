9f8f0eacb6b8fcc54a6ddfcf025cd6af4824acb6
Ronie083 <ahmedronie6@gmail.com>
2023-07-20 18:50:41 +0600

    Animated some part

--- src/Components/Preloader/Preloader.jsx
+++ src/Components/Preloader/Preloader.jsx
@@ -1,34 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
 import anim from '../../../public/animation_lkb3a6mv.json';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Preloader = () => {
-  const [percentage, setPercentage] = useState(0);
+    const [percentage, setPercentage] = useState(0);
 
-  useEffect(() => {
-    const targetPercentage = 100;
-    const animationDuration = 3000;
-    const increment = (targetPercentage / animationDuration) * 10;
 
-    const interval = setInterval(() => {
-      setPercentage((prevPercentage) => {
-        const newPercentage = prevPercentage + increment;
-        return newPercentage >= targetPercentage ? targetPercentage : newPercentage;
-      });
-    }, 10);
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        AOS.init();
+    }, []);
 
-  return (
-    <div className="text-center grid md:grid-cols-2">
-      <div className='text-3xl max-h-fit md:text-9xl flex items-center justify-center'>
-        <h1>{percentage.toFixed(0)}%</h1>
-      </div>
-      <div className="py-32 md:py-0">
-        <Lottie animationData={anim} loop={true} />
-      </div>
-    </div>
-  );
+    useEffect(() => {
+        const targetPercentage = 100;
+        const animationDuration = 3000;
+        const increment = (targetPercentage / animationDuration) * 10;
+
+        const interval = setInterval(() => {
+            setPercentage((prevPercentage) => {
+                const newPercentage = prevPercentage + increment;
+                return newPercentage >= targetPercentage ? targetPercentage : newPercentage;
+            });
+        }, 10);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="text-center grid md:grid-cols-2" data-aos="fade-left">
+            <div className='text-3xl max-h-fit md:text-9xl flex items-center justify-center'>
+                <h1>{percentage.toFixed(0)}%</h1>
+            </div>
+            <div className="py-32 md:py-0">
+                <Lottie animationData={anim} loop={true} />
+            </div>
+        </div>
+    );
 };
 
 export default Preloader;
